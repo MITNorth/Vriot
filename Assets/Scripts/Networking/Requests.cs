b73a0595f1e126cc209f39c7a821cef695f3b16c
Zhehai Zhang <zhehaizhang1@gmail.com>
2023-01-15 03:42:06 -0500

    donezo

--- Assets/Scripts/Networking/Requests.cs
+++ Assets/Scripts/Networking/Requests.cs
@@ -17,10 +17,7 @@ namespace Requests
 {
     public class LightsRequest
     {
-        private EventSourceReader source;
         private string url;
-        public delegate void EventHandler(object sender, EventSourceMessageEventArgs e);
-        public bool isNetworkError = false;
         public class LightUpdateData
         {
             public string state;
@@ -69,6 +66,107 @@ namespace Requests
 
     }
 
+    // lgiths, doorbell, alarm, open door, 
+
+    public class AlarmRequest
+    {
+        private string url;
+        public class AlarmUpdateData
+        {
+            public string state;
+        }
+
+        public AlarmRequest()
+        {
+            url = Globals.API_URL + "/" + Globals.VERSION + "/" + Globals.PREAMBLE + "/" + "alarm_module_event";
+        }
+
+        private IEnumerator Post(UnityWebRequest req)
+        {
+            yield return req.SendWebRequest();
+
+            if (req.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Error");
+            }
+            else
+            {
+                Debug.Log("Updated alarm");
+            }
+            req.Dispose();
+            
+        }
+
+        public IEnumerator ToggleAlarm(bool state)
+        {
+            Debug.Log("Alarm Request Started");
+            var alarmData = new AlarmUpdateData();
+            alarmData.state = state ? "on" : "off";
+            string json = JsonUtility.ToJson(alarmData);
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+
+            var request = UnityWebRequest.Post(url, new WWWForm());
+            Debug.Log(url);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("x-api-key", "API_KEY");
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            return Post(request);
+            // return this;
+        }
+
+    }
+
+    public class DoorRequest
+    {
+        private string url;
+        public class DoorUpdateData
+        {
+            public string state;
+        }
+
+        public DoorRequest()
+        {
+            url = Globals.API_URL + "/" + Globals.VERSION + "/" + Globals.PREAMBLE + "/" + "door_module_event";
+        }
+
+        private IEnumerator Post(UnityWebRequest req)
+        {
+            yield return req.SendWebRequest();
+
+            if (req.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Error");
+            }
+            else
+            {
+                Debug.Log("Updated door");
+            }
+            req.Dispose();
+            
+        }
+
+        public IEnumerator ToggleDoor(bool state)
+        {
+            Debug.Log("Door Request Started");
+            var doorData = new DoorUpdateData();
+            doorData.state = state ? "on" : "off";
+            string json = JsonUtility.ToJson(doorData);
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+
+            var request = UnityWebRequest.Post(url, new WWWForm());
+            Debug.Log(url);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("x-api-key", "API_KEY");
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            return Post(request);
+            // return this;
+        }
+
+    }
     public class TempSensor
     {
 
